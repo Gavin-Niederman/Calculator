7fd5056ee31894a30f44c71993282649c26fcddf
Gavin Niederman <gavinniederman@gmail.com>
2022-04-05 20:30:00 -0700

    Add basic types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+enum TokenType {
+    LeftParen,
+    RightParen,
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Number(i128)
+} 
+
+struct Token {
+    tokentype: TokenType,
+    location: i32,
+}
+
+struct Scanner {
+    equation: String,
+    tokens: Vec<Token>,
+    start: i32,
+    current: i32
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+impl Scanner {
+    fn scan_tokens(&mut self) {
+        
+    }
 }
