8e94da8e4b5a935ea30db04f88b21d78f76a6781
Gavin Niederman <gavinniederman@gmail.com>
2022-04-05 21:10:54 -0700

    scanning excluding numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{str::Chars, panic::Location};
+
 enum TokenType {
     LeftParen,
     RightParen,
@@ -26,6 +28,28 @@ fn main() {
 
 impl Scanner {
     fn scan_tokens(&mut self) {
-        
+        let clone = self.equation.clone();
+        let mut chars = clone.chars();
+
+        while chars.clone().next() != None {
+            self.current += 1;
+            self.start = self.current;
+            match chars.next().unwrap() {
+                '(' => self.add_token(TokenType::LeftParen),
+                ')' => self.add_token(TokenType::RightParen),
+                '+' => self.add_token(TokenType::Add),
+                '-' => self.add_token(TokenType::Subtract),
+                '*' => self.add_token(TokenType::Multiply),
+                '/' => self.add_token(TokenType::Divide),
+                '0'..='9' => {}//whyyy
+                other => if other != ' ' {
+                    eprintln!("Unexpected character {} at charachter {}", other, self.current);
+                }
+            }
+        }
+    }
+
+    fn add_token(&mut self, tokentype: TokenType) {
+        self.tokens.push(Token{tokentype, location: self.current})
     }
 }
