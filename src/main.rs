972f21a206599c229a6e202b78e811f65cf1e819
Gavin Niederman <gavinniederman@gmail.com>
2022-04-06 20:58:32 -0700

    added basic scanning

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{str::Chars, panic::Location};
+use std::str::Chars;
 
+#[derive(Debug)]
 enum TokenType {
     LeftParen,
     RightParen,
@@ -7,9 +8,10 @@ enum TokenType {
     Subtract,
     Multiply,
     Divide,
-    Number(i128)
-} 
+    Number(i128),
+}
 
+#[derive(Debug)]
 struct Token {
     tokentype: TokenType,
     location: i32,
@@ -19,11 +21,12 @@ struct Scanner {
     equation: String,
     tokens: Vec<Token>,
     start: i32,
-    current: i32
+    current: i32,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut scanner = Scanner::new("1 + 1".to_string());
+    scanner.scan_tokens();
 }
 
 impl Scanner {
@@ -41,15 +44,47 @@ impl Scanner {
                 '-' => self.add_token(TokenType::Subtract),
                 '*' => self.add_token(TokenType::Multiply),
                 '/' => self.add_token(TokenType::Divide),
-                '0'..='9' => {}//whyyy
-                other => if other != ' ' {
-                    eprintln!("Unexpected character {} at charachter {}", other, self.current);
+                current @ '0'..='9' => {
+                    self.form_numbers(current, &mut chars)
+                }
+                other => {
+                    if other != ' ' {
+                        eprintln!(
+                            "Unexpected character {} at charachter {}",
+                            other, self.current
+                        );
+                    }
                 }
             }
         }
+        println!("{:?}", self.tokens);
     }
 
     fn add_token(&mut self, tokentype: TokenType) {
-        self.tokens.push(Token{tokentype, location: self.current})
+        self.tokens.push(Token {
+            tokentype,
+            location: self.current,
+        })
+    }
+
+    fn new(equation: String) -> Scanner {
+        Scanner { equation, tokens: Vec::new(), start: 0, current: 0 }
+    }
+
+    fn form_numbers(&mut self, current: char, chars: &mut Chars) {
+        let mut number = String::from(current.to_string());
+        while chars.clone().next() != None {
+            if let '0'..='9' = chars.clone().next().unwrap() {
+                self.current += 1;
+                number += &chars.next().unwrap().to_string();  
+            } else {
+                break;
+            }
+        }
+        self.add_token(TokenType::Number(
+            number
+                .parse()
+                .expect("Internal error! Failed to parse number"),
+        ))
     }
 }
